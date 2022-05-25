3b2db78d4f7ae7322d0f76c9023fbc0ed0a8d58b
Rajukst <rajuahamedkst15@gmail.com>
2022-05-25 16:42:10 +0600

    setup complete

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yyhry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("RadioStation");
+    const RadioStations = database.collection("Station-List");
+
+    // creating add doctors bio
+    app.post("/new-station", async (req, res) => {
+      const addStation = req.body;
+      const getStation = await RadioStations.insertOne(addStation);
+      console.log("getting a new radio station", getStation);
+      res.json(getStation);
+      console.log(getStation);
+    });
+
+
+  
+
+  } finally {
+    // client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Project Server Is Running");
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
